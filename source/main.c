b4e5e2f4aae52965aed946f13e9a67b8c9151b51
Ubuntu <datonearash@gmail.com>
2020-12-05 16:34:14 -0800

    Initializing repository

---
+++ source/main.c
@@ -0,0 +1,63 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <bit.h>
+#include <timer.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum Demo_States {shift};
+int Demo_Tick(int state){
+		// Local Variables
+	static unsigned char pattern = 0x80;	// LED pattern - 0: LED off; 1: LED on
+	static unsigned char row = 0xFE;  	// Row(s) displaying pattern.
+							// 0: display pattern on row
+							// 1: do NOT display pattern on row
+	// Transitions
+	switch (state) {
+		case shift:
+			break;
+		default:
+			state = shift;
+			break;
+	}
+	// Actions
+	switch (state) {
+		case shift:
+			if (row == 0xEF && pattern == 0x01) { // Reset demo
+				pattern = 0x80;
+				row = 0xFE;
+			} else if (pattern == 0x01) { // Move LED to start of next row
+				pattern = 0x80;
+				row = (row << 1) | 0x01;
+			} else { // Shift LED one spot to the right on current row
+				pattern >>= 1;
+			}
+			break;
+		default:
+			break;
+	}
+	PORTC = pattern;	// Pattern to display
+	PORTD = row;		// Row(s) displaying pattern
+	return state;
+}
+
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+    /* Insert your solutiion below */
+    while (1) {
+	Demo_Tick(0);
+    }
+    return 1;
+}
